e1c387d0a3ede3f4b9fe86ee38b7c8ae16c5748f
mustafa72625 <147307829+mustafa72625@users.noreply.github.com>
2024-08-02 21:20:16 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+$TOKEN = ""; // bot token
+$botUsername = ""; // bot username without @
+
+/*
+$databaseHost = "localhost";
+$databaseName = ""; // db name
+$databaseUsername = ""; // db username
+$databasePassword = ""; // db password
+*/
+
+$sudoID = ""; // sudo id
+
+date_default_timezone_set("Asia/Riyadh");
